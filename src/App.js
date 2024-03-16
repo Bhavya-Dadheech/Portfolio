9ceb633ac35f092a4897c91e3723062c27b20774
Bhavya Dadheech <70031238+Bhavya-Dadheech@users.noreply.github.com>
2024-03-16 15:12:32 +0530

    build update

--- src/App.js
+++ src/App.js
@@ -18,42 +18,12 @@ function App() {
   const scrollToRef = (ref) => {
     let yOffset = -100; // Default yOffset
 
-    // Check viewport width
-    if (window.innerWidth < 768 && ref !== homeRef) {
-      yOffset = -350; // Adjusted yOffset for smaller screens (you can adjust this value as needed)
-      console.log("not here");
-    }
-
     const y = ref.current.getBoundingClientRect().top + window.scrollY + yOffset;
     window.scrollTo({ top: y, behavior: "smooth" });
   };
 
-  // const [vantaEffect, setVantaEffect] = useState(null);
-  // const myRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (!vantaEffect) {
-  //     setVantaEffect(
-  //       NET({
-  //         el: myRef.current,
-  //         mouseControls: true,
-  //         touchControls: true,
-  //         gyroControls: false,
-  //         scale: 1.0,
-  //         scaleMobile: 1.0
-  //       })
-  //     );
-  //   }
-
-  //   return () => {
-  //     if (vantaEffect) vantaEffect.destroy();
-  //   };
-  // }, [vantaEffect]);
-
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      {/* <div className="vanta-container" ref={myRef}></div> */}
-
       {/* NavBar */}
       <Navbar
         scrollToRef={scrollToRef}
